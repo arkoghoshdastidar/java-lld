b32816ff8ed8b067532fde2a07f97d2651304a82
Arko Ghosh Dastidar <arkoghoshdastidar392000@gmail.com>
2026-06-04 00:46:47 +0530

    updated Arrays.java file

--- Arrays.java
+++ Arrays.java
@@ -4,6 +4,6 @@ public class ArrayCopyExample {
     public static void main(String[] args) {
         int[] copyFrom = {1, 2, 3, 4, 5, 6};
         int[] copyTo = Arrays.copyOfRange(copyFrom, 2, 5);
-        System.out.println("copyTo: " + Arrays.toString(copyTo));
+        Arrays.stream(copyTo).forEach(System.out::println);
     }
 }
