f8a343c219e8b069aae6b4d2d179f381c37b2330
Arko Ghosh Dastidar <arkoghoshdastidar392000@gmail.com>
2026-07-01 23:50:15 +0530

    restructured the project directory

--- Arrays.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class ArrayCopyExample {
-    public static void main(String[] args) {
-        int[] copyFrom = {1, 2, 3, 4, 5, 6};
-        int[] copyTo = Arrays.copyOfRange(copyFrom, 2, 5);
-        Arrays.stream(copyTo).forEach(System.out::println);
-    }
-}
